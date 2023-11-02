675c580a8c144bee93b37aba2c1798f3e1b12c7c
audiESP <72719332+audiESP@users.noreply.github.com>
2023-11-02 23:00:14 +1100

    Update Eric Base

---
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/ScreenshotSaver.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Simple screenshot saver for FIT3169
+
+/* Usage:
+ 	- Any time the '0' key is pressed, a screenshot will save into your project folder.
+		- Navigate to your project in Explorer or Finder and the screenshots will be next to the assets folder.
+ */
+
+public class ScreenshotSaver : MonoBehaviour {
+
+	public KeyCode captureKey = KeyCode.Alpha0;
+	public int upscaleFactor = 1;
+	private int screenshotCount = 0;
+	
+	void Update()
+	{
+		if (Application.isEditor && Input.GetKeyDown(captureKey))
+		{        
+			string screenshotFilename;
+			do
+			{
+				// Increment suffix so we don't overwrite an existing image
+				screenshotCount++;
+				screenshotFilename = "Promo Screenshot " + screenshotCount + ".png";
+				
+			} while (System.IO.File.Exists(screenshotFilename));
+			
+			ScreenCapture.CaptureScreenshot(screenshotFilename, upscaleFactor);
+		}
+	}
+}
